8f8641aec31829b3d0ed447ef5b8eeb15c65fcc3
wbe <34304532+wbecerro@users.noreply.github.com>
2025-09-15 17:45:20 +0200

    Añadida la opción de poder crear recetas sin forma

--- src/main/java/wbe/hephaestusForge/util/RecipeManager.java
+++ src/main/java/wbe/hephaestusForge/util/RecipeManager.java
@@ -6,6 +6,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.RecipeChoice;
 import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.ShapelessRecipe;
 import wbe.hephaestusForge.HephaestusForge;
 
 import java.util.*;
@@ -46,13 +47,29 @@ public class RecipeManager {
         keys.add(recipeKey);
     }
 
+    public void loadShapelessRecipe(String id, HashMap<ItemStack, Integer> ingredients) {
+        NamespacedKey recipeKey = new NamespacedKey(plugin, id + "recipe");
+        ShapelessRecipe recipe = new ShapelessRecipe(recipeKey, getResult(id));
+        for(Map.Entry<ItemStack, Integer> ingredient : ingredients.entrySet()) {
+            for(int i=0;i<ingredient.getValue();i++) {
+                recipe.addIngredient(new RecipeChoice.ExactChoice(ingredient.getKey()));
+            }
+        }
+
+        plugin.getServer().addRecipe(recipe);
+        keys.add(recipeKey);
+    }
+
     private ItemStack getResult(String id) {
         String type = recipeConfig.getString("Recipes." + id + ".result.type");
         String material = recipeConfig.getString("Recipes." + id + ".result.item");
+        int amount = recipeConfig.getInt("Recipes." + id + ".result.amount");
         if(type.equalsIgnoreCase("material")) {
-            return new ItemStack(Material.valueOf(material));
+            return new ItemStack(Material.valueOf(material), amount);
         } else {
-            return HephaestusForge.config.savedItems.get(material);
+            ItemStack item = HephaestusForge.config.savedItems.get(material);
+            item.setAmount(amount);
+            return item;
         }
     }
 
@@ -60,20 +77,38 @@ public class RecipeManager {
         Set<String> configRecipes = recipeConfig.getConfigurationSection("Recipes").getKeys(false);
         for(String recipe : configRecipes) {
             String id = recipe;
-            String[] shape = recipeConfig.getStringList("Recipes." + recipe + ".shape").toArray(new String[3]);
-            HashMap<Character, ItemStack> ingredients = new HashMap<>();
-            Set<String> configIngredients = recipeConfig.getConfigurationSection("Recipes." + recipe + ".ingredients").getKeys(false);
-            for(String ingredient : configIngredients) {
-                String material = recipeConfig.getString("Recipes." + recipe + ".ingredients." + ingredient + ".item");
-                String type = recipeConfig.getString("Recipes." + recipe + ".ingredients." + ingredient + ".type");
-                if(type.equalsIgnoreCase("material")) {
-                    ingredients.put(ingredient.charAt(0), new ItemStack(Material.valueOf(material)));
-                } else if(type.equalsIgnoreCase("item")) {
-                    ingredients.put(ingredient.charAt(0), HephaestusForge.config.savedItems.get(material));
+            String shapeType = recipeConfig.getString("Recipes." + recipe + ".type");
+            if(shapeType.equalsIgnoreCase("shaped")) {
+                String[] shape = recipeConfig.getStringList("Recipes." + recipe + ".shape").toArray(new String[3]);
+                HashMap<Character, ItemStack> ingredients = new HashMap<>();
+                Set<String> configIngredients = recipeConfig.getConfigurationSection("Recipes." + recipe + ".ingredients").getKeys(false);
+                for(String ingredient : configIngredients) {
+                    String material = recipeConfig.getString("Recipes." + recipe + ".ingredients." + ingredient + ".item");
+                    String type = recipeConfig.getString("Recipes." + recipe + ".ingredients." + ingredient + ".type");
+                    if(type.equalsIgnoreCase("material")) {
+                        ingredients.put(ingredient.charAt(0), new ItemStack(Material.valueOf(material)));
+                    } else if(type.equalsIgnoreCase("item")) {
+                        ingredients.put(ingredient.charAt(0), HephaestusForge.config.savedItems.get(material));
+                    }
                 }
-            }
 
-            loadShapedRecipe(id, shape, ingredients);
+                loadShapedRecipe(id, shape, ingredients);
+            } else if(shapeType.equalsIgnoreCase("shapeless")) {
+                HashMap<ItemStack, Integer> ingredients = new HashMap<>();
+                Set<String> configIngredients = recipeConfig.getConfigurationSection("Recipes." + recipe + ".ingredients").getKeys(false);
+                for(String ingredient : configIngredients) {
+                    String material = recipeConfig.getString("Recipes." + recipe + ".ingredients." + ingredient + ".item");
+                    String type = recipeConfig.getString("Recipes." + recipe + ".ingredients." + ingredient + ".type");
+                    int amount = recipeConfig.getInt("Recipes." + recipe + ".ingredients." + ingredient + ".amount");
+                    if(type.equalsIgnoreCase("material")) {
+                        ingredients.put(new ItemStack(Material.valueOf(material)), amount);
+                    } else if(type.equalsIgnoreCase("item")) {
+                        ingredients.put(HephaestusForge.config.savedItems.get(material), amount);
+                    }
+                }
+
+                loadShapelessRecipe(id, ingredients);
+            }
         }
     }
 }
